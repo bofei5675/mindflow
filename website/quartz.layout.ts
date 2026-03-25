dac64d00b06cbd12d115a82c844645a0701be8b9
Qing Li <dylan.liqing@gmail.com>
2026-03-25 11:57:46 +0800

    feat: remove top nav, move PageTitle back to left sidebar

--- website/quartz.layout.ts
+++ website/quartz.layout.ts
@@ -4,10 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.PageTitle(),
-    Component.TopNav(),
-  ],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -25,6 +22,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
+    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
